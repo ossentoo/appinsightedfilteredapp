80a036d0b79bba0c5f4d624518fb330f8705db5a
Oscar Ssentoogo <ossentoogo@nephilaadvisors.co.uk>
2020-07-24 14:32:00 +0100

    working app insights filter and unit tests

--- FrontDoorAppInsightsFilteredApp/Program.cs
+++ FrontDoorAppInsightsFilteredApp/Program.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -20,7 +15,25 @@ namespace FrontDoorAppInsightsFilteredApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder
+                    .UseStartup<Startup>()
+                    .ConfigureLogging(
+                                builder =>
+                                {
+                                    // Providing an instrumentation key here is required if you're using
+                                    // standalone package Microsoft.Extensions.Logging.ApplicationInsights
+                                    // or if you want to capture logs from early in the application startup
+                                    // pipeline from Startup.cs or Program.cs itself.
+                                    builder.AddApplicationInsights("ikey");
+
+                                    // Optional: Apply filters to control what logs are sent to Application Insights.
+                                    // The following configures LogLevel Information or above to be sent to
+                                    // Application Insights for all categories.
+                                    builder.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>
+                                                    ("", LogLevel.Information);
+                                }
+                            );                    
+                })                
+                ;
     }
 }
